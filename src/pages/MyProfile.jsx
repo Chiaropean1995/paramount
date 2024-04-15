df1b4619553c6cb3797d7c9470efc2f31fbb12d4
chiaropean1995 <chiaropean1995@gmail.com>
2024-04-16 07:58:06 +0800

    my profile

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import Header from "../components/Header"
 import Navbar1 from "../components/Navbar1"
 import axios from "axios"
@@ -8,22 +8,7 @@ import AddProfileImage from "../components/AddProfileImage"
 export default function ProfilePage() {
     const url = "https://paramount-i0x2.onrender.com";
     const [images, setImages] = useState([]);
-    const [loading, setLoading] = useState(true);
 
-
-    const fetchImages = async () => {
-        try {
-            const response = await axios.get(`${url}/userimage`);
-            setImages(response.data);
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching images:', error);
-        }
-    };
-
-    useEffect(() => {
-        fetchImages();
-    }, []);
 
     const handleAddProfileImage = async (newImage) => {
         try {
