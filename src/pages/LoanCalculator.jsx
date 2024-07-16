d5b66cd398069e22cb399c0a0c4e5624ce96b822
chiaropean1995 <chiaropean1995@gmail.com>
2024-07-16 13:59:43 +0800

    loan calculator

--- src/pages/LoanCalculator.jsx
+++ src/pages/LoanCalculator.jsx
@@ -53,38 +53,27 @@ const PaymentCalculator = () => {
             <Header />
             <Navbar1 />
             <Container fluid>
-                <Row>
-                    <Col sm={12} className="p-0 position-relative"  >
-                        <img src={picture2} alt="Your image" height="250px" className="w-100 " style={{ filter: 'brightness(0.3)' }}></img>
-                    </Col>
-                    <h2 style={{ fontFamily: '"Source Sans Pro", sans-serif', left: '60px', top: '250px' }} className="font-weight-bold position-absolute text-white ">LOAN CALCULATOR</h2>
-
-                </Row>
-            </Container>
-            <div>
-                <div style={{ marginTop: "100px", display: "flex", justifyContent: "center" }}>
-                    <Row className="mx-2">
-                        <video autoPlay muted loop id="video-bg">
-                            <source src={video} type="video/mp4" />
-                        </video>
-                        <Col sm={12} md={5} className="p-4 border rounded-start p-5 " style={{ boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)" }}  >
-                            <h4 style={{ fontFamily: "Nunito, Roboto, sans-serif" }}>House Budget Calculator</h4>
-                            <Form>
-                                <Form.Group controlId="netIncome">
-                                    <Form.Label>Net Income (RM):</Form.Label>
-                                    <Form.Control type="text" placeholder="RM" value={netIncome} onChange={(e) => setNetIncome(e.target.value)} />
+                <Row className="justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+                    <video autoPlay muted loop id="video-bg" style={{ position: "absolute", width: "100%", height: "100%", objectFit: "cover", zIndex: "-1" }}>
+                        <source src={video} type="video/mp4" />
+                    </video>
+                    <Col sm={12} md={6} className="p-4 border rounded p-5 mt-5" style={{ backgroundColor: "rgba(255, 255, 255, 0.9)", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)" }}>
+                        <h4 style={{ fontFamily: "Nunito, Roboto, sans-serif" }}>House Budget Calculator</h4>
+                        <Form>
+                            <Form.Group controlId="netIncome">
+                                <Form.Label>Net Income (RM):</Form.Label>
+                                <Form.Control type="text" placeholder="RM" value={netIncome} onChange={(e) => setNetIncome(e.target.value)} />
+                            </Form.Group>
+                            <div className="d-flex">
+                                <Form.Group controlId="commitment">
+                                    <Form.Label>Commitment (RM):</Form.Label>
+                                    <Form.Control type="text" placeholder="RM" value={commitment} onChange={(e) => setCommitment(e.target.value)} />
                                 </Form.Group>
-                                <div className="d-flex">
-                                    <Form.Group controlId="commitment">
-                                        <Form.Label>Commitment (RM):</Form.Label>
-                                        <Form.Control type="text" placeholder="RM" value={commitment} onChange={(e) => setCommitment(e.target.value)} />
-                                    </Form.Group>
-                                    <Form.Group controlId="dsr" className="ms-3">
-                                        <Form.Label>DSR by Bank (%):</Form.Label>
-                                        <Form.Control type="text" placeholder="%" value={dsr} onChange={(e) => setDsr(e.target.value)} />
-                                    </Form.Group>
-                                </div>
-                            </Form>
+                                <Form.Group controlId="dsr" className="ms-3">
+                                    <Form.Label>DSR by Bank (%):</Form.Label>
+                                    <Form.Control type="text" placeholder="%" value={dsr} onChange={(e) => setDsr(e.target.value)} />
+                                </Form.Group>
+                            </div>
                             <div className="text-center mt-3">
                                 <Button variant="primary" onClick={calculateHouseBudget}>Calculate</Button>
                             </div>
@@ -109,46 +98,10 @@ const PaymentCalculator = () => {
                                     <Button variant="primary" onClick={handleCalculate}>Calculate</Button>
                                 </div>
                             </Form>
-                        </Col>
-                        <Col sm={12} md={7} className="p-4 border rounded-end p-5 box-shadow" style={{ backgroundColor: " #5c85d6", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)" }} >
-                            <div>
-                                <h5 className="text-white" style={{ fontFamily: "Nunito, Roboto, sans-serif", marginBottom: "10px" }}>Summary:</h5>
-                                <div style={{ marginBottom: "20px" }}>
-                                    <div style={{ marginBottom: "200px" }}>
-                                        <span className="text-white" style={{ color: "#505050", fontSize: "16px", fontWeight: "bold" }}>Estimated House Budget:</span>
-                                        <span style={{ marginLeft: "10px", fontWeight: "bold" }}>{houseBudget ? `RM ${addCommas(houseBudget)}` : ""}</span>
-
-                                    </div>
-
-                                    <div>
-                                        <span className="text-white" style={{ color: "#505050", fontSize: "16px", fontWeight: "bold" }}>Monthly Payment:</span>
-                                        <span style={{ marginLeft: "10px", fontWeight: "bold" }}>{monthlyPayment ? `RM ${addCommas(monthlyPayment)}` : ""}</span>
-                                    </div>
-                                </div>
-                                <div className="d-flex align-items-center">
-                                    {principalPercentage > 0 && (
-                                        <div style={{ width: `${principalPercentage}%`, height: "20px", backgroundColor: "#77CEE2", position: 'relative', textAlign: 'center' }}>
-                                            <span style={{ position: "absolute", top: "50%", transform: "translate(-50%, -50%)", color: "white", fontSize: "12px", fontWeight: "bold" }}>{principalPercentage}%</span>
-                                        </div>
-                                    )}
-                                    {interestPercentage > 0 && (
-                                        <div style={{ width: `${interestPercentage}%`, height: "20px", backgroundColor: "#AFD5DE", position: 'relative', textAlign: 'center' }}>
-                                            <span style={{ position: "absolute", top: "50%", transform: "translate(-50%, -50%)", color: "white", fontSize: "12px", fontWeight: "bold" }}>{interestPercentage}%</span>
-                                        </div>
-                                    )}
-                                </div>
-                                <div className="d-flex mt-3">
-                                    <div className="rounded-circle me-3" style={{ backgroundColor: "#77CEE2", width: '20px', height: '20px' }}></div>
-                                    <span className="text-white">Principal Amount</span>
-                                    <div className="rounded-circle ms-5 me-3" style={{ backgroundColor: "#AFD5DE", width: '20px', height: '20px' }}></div>
-                                    <span className="text-white ">Interest Amount</span>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                </div>
-
-            </div >
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
             <Footer />
         </>
     );
